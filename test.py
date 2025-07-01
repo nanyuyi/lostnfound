db3d8ef233532d3e3c71bac7c44b3e631909a8d4
userName <userEmail>
2025-07-01 18:06:29 +0800

    加了一个简单的注册

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 #执行 flask --app test run --host=0.0.0.0(监听所有IP)
-from flask import Flask, send_from_directory, request
+from flask import Flask, send_from_directory, request,render_template
 import hashlib
 
 import user
@@ -8,7 +8,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, world!</p>"
+    return '''
+        <h1>欢迎！</h1>
+        <form action="/register_page">
+            <button type="submit">去注册</button>
+        </form>
+    '''
+@app.route("/register_page")
+def register_page():
+    return render_template("register.html")
+
 
 @app.route('/favicon.ico')
 def favicon():
@@ -16,17 +25,19 @@ def favicon():
         app.root_path, 'favicon.ico', mimetype='image/vnd.microsoft.icon'
     )
 
+
+#注册
 @app.route("/register", methods=["POST"])
 def register():
-    userid = request.form('userid')
-    password = request.form('password')
+    userid = request.form['username']
+    password = request.form['password']
     if not userid or not password:
         return "Invalid input", 400
     
-    hashed_userid = hashlib.sha256(userid.encode())
-    hashed_password = hashlib.sha256(password.encode())
+    hashed_userid = hashlib.sha256(userid.encode()).hexdigest()
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
     if user.register_user(hashed_password, hashed_userid):
         return "User registered successfully", 201
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- user.py
+++ user.py
@@ -3,17 +3,27 @@ import sqlite3
 con = sqlite3.connect('user.db')
 cur = con.cursor()
 
-cur.execute('''
-CREATE TABLE IF NOT EXISTS users (
-    userid TEXT PRIMARY KEY NOT NULL,
-    password TEXT NOT NULL
-);
-''')
+def init_user():
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        userid TEXT PRIMARY KEY NOT NULL,
+        password TEXT NOT NULL
+    );
+    ''')
 
 def register_user(userid, password):
+    con = sqlite3.connect('user.db')
+    cur = con.cursor()
     try:
         cur.execute('INSERT INTO users (userid, password) VALUES (?, ?)', (userid, password))
         con.commit()
         return True
     except sqlite3.IntegrityError:
         return False
+    
+if __name__ == "__main__":
+    con = sqlite3.connect('user.db')
+    cur = con.cursor()
+    res = cur.execute('SELECT * FROM users;')
+    for row in res:
+        print(row)
